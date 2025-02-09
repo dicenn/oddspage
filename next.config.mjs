2e303a0e4d3cb7ea3e90bdadefd21daaf240664b
softgenai <admin@softgen.ai>
2025-02-09 19:15:54 +0000

    Edit next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   webpack: (config) => {
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
       'bufferutil': 'commonjs bufferutil',
+      'ws': 'commonjs ws'
     })
     return config
   },
+  experimental: {
+    esmExternals: 'loose'
+  }
 }
 
 export default nextConfig
